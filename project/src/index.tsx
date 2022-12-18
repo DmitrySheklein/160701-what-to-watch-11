f3d7f857357c9399be4591ecb3dff500a5b37d5c
Дмитрий <shcheklein.dev@gmail.com>
2022-12-18 18:41:46 +0300

    Fix: стили проекта

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -6,7 +6,7 @@ import { store } from './store';
 import { checkAuthAction, fetchFilmsAction } from 'src/store/api-actions';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import './index.css';
+import './styles/index.css';
 
 store.dispatch(checkAuthAction());
 store.dispatch(fetchFilmsAction());
