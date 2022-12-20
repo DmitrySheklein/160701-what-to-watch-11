56d56d0ec16c654830f577ffc63f5747ca814edd
Dmitry <sheklein.dima@gmail.com>
2022-12-20 14:45:10 +0300

    Feat: location-path.test.tsx

--- project/src/hooks/location-path/location-path.test.tsx
+++ project/src/hooks/location-path/location-path.test.tsx
@@ -1,13 +1,77 @@
 import { renderHook } from '@testing-library/react';
 import useCurrentLocation from './location-path';
+import router from 'react-router-dom';
+import { AppRoute, RouteName } from 'src/const';
 
-describe('Hook: useCurrentLocation', () => {
+type TRouter = typeof router;
+
+const mockUseLocationValue = {
+  pathname: '/',
+  search: '',
+  hash: '',
+  key: '',
+  state: null,
+};
+
+jest.mock(
+  'react-router-dom',
+  (): TRouter => ({
+    ...jest.requireActual('react-router-dom'),
+    useLocation: () => mockUseLocationValue,
+  }),
+);
+
+describe('ONLY Hook: useCurrentLocation', () => {
   it('should return object with 5 elements', () => {
-    const { result } = renderHook(() => useCurrentLocation());
-    // console.log(result);
-    // const [answers, handleAnswerChange] = result.current;
-    // expect(result.current).toHaveLength(2);
-    // expect(answers).toBeInstanceOf(Array);
-    // expect(handleAnswerChange).toBeInstanceOf(Function);
+    const {
+      result: { current },
+    } = renderHook(() => useCurrentLocation());
+
+    // expect(current).toHaveLength(5);
+    expect(current).toBeInstanceOf(Object);
+    expect(current).toHaveProperty('isRootPage');
+    expect(current.isRootPage).toBe(true);
+  });
+  it(`should return isRootPage TRUE if ${AppRoute.Root} `, () => {
+    mockUseLocationValue.pathname = AppRoute.Root;
+    const {
+      result: { current },
+    } = renderHook(() => useCurrentLocation());
+
+    expect(current).toHaveProperty('isRootPage');
+    expect(current.isRootPage).toBe(true);
+  });
+  it(`should return isLoginPage TRUE if ${AppRoute.Login} `, () => {
+    mockUseLocationValue.pathname = AppRoute.Login;
+
+    const {
+      result: { current },
+    } = renderHook(() => useCurrentLocation());
+
+    expect(current).toHaveProperty('isLoginPage');
+    expect(current.isLoginPage).toBe(true);
+    expect(current.isRootPage).toBe(false);
+  });
+  it(`should return isAddReviewPage TRUE if ${AppRoute.AddReview} `, () => {
+    mockUseLocationValue.pathname = `${RouteName.Films}/1/${RouteName.Review}`;
+
+    const {
+      result: { current },
+    } = renderHook(() => useCurrentLocation());
+
+    expect(current).toHaveProperty('isAddReviewPage');
+    expect(current.isAddReviewPage).toBe(true);
+    expect(current.isRootPage).toBe(false);
+  });
+  it(`should return isMoviePage TRUE if ${AppRoute.Film} `, () => {
+    mockUseLocationValue.pathname = `${RouteName.Films}/1`;
+
+    const {
+      result: { current },
+    } = renderHook(() => useCurrentLocation());
+
+    expect(current).toHaveProperty('isMoviePage');
+    expect(current.isMoviePage).toBe(true);
+    expect(current.isAddReviewPage).toBe(false);
   });
 });
