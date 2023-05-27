9b39a8abdc8c03f0eaabfdb31f61211893a44fab
Дмитрий <shcheklein.dev@gmail.com>
2023-05-27 22:54:59 +0300

    Fix: breadcrumbs.test.tsx

--- project/src/components/breadcrumbs/breadcrumbs.test.tsx
+++ project/src/components/breadcrumbs/breadcrumbs.test.tsx
@@ -2,12 +2,15 @@ import { render, screen } from '@testing-library/react';
 import Breadcrumbs from './breadcrumbs';
 import { FakeAppTemplate } from '../app/app.test';
 import { AuthStatus } from 'src/const';
+import { makeFakeFilm } from 'src/utils/mocks';
+const fakeFilm = makeFakeFilm();
+
 
 describe('Component: AddReviewForm', () => {
   it('should render correctly', () => {
     render(
       <FakeAppTemplate storeType={AuthStatus.Auth}>
-        <Breadcrumbs />
+        <Breadcrumbs film={fakeFilm} />
       </FakeAppTemplate>,
     );
 
