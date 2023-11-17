2d333ae09f197076fcbe01c74f46b4ad9457982f
Dmitry <shcheklein.dev@gmail.com>
2023-11-17 20:44:38 +0300

    fix: axios instance export

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,31 +12,30 @@ const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMap[response
 const BACKEND_URL = 'https://11.react.pages.academy/wtw';
 const REQUEST_TIMEOUT = 5000;
 
-export const createApi = (): AxiosInstance => {
-  const api = axios.create({
-    baseURL: BACKEND_URL,
-    timeout: REQUEST_TIMEOUT,
-  });
+const api = axios.create({
+  baseURL: BACKEND_URL,
+  timeout: REQUEST_TIMEOUT,
+});
 
-  api.interceptors.request.use((config: AxiosRequestConfig) => {
-    const token = getToken();
+api.interceptors.request.use((config: AxiosRequestConfig) => {
+  const token = getToken();
 
-    if (token && config.headers) {
-      config.headers['x-token'] = token;
+  if (token && config.headers) {
+    config.headers['x-token'] = token;
+  }
+
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError<{ error: string }>) => {
+    if (error.response && shouldDisplayError(error.response)) {
+      toast.error(error.response.data.error);
     }
 
-    return config;
-  });
+    throw error;
+  },
+);
 
-  api.interceptors.response.use(
-    (response) => response,
-    (error: AxiosError<{ error: string }>) => {
-      if (error.response && shouldDisplayError(error.response)) {
-        toast.error(error.response.data.error);
-      }
-
-      throw error;
-    },
-  );
-  return api;
-};
+export { api };
